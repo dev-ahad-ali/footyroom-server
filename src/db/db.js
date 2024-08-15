12d487f618d10bcb5334a25be49fb88400e1d059
dev-ahad-ali <ahadali.dev@gmail.com>
2024-08-16 00:23:33 +0600

    Update: database connection method created

--- src/db/db.js
+++ src/db/db.js
@@ -1,8 +1,12 @@
 import mongoose from 'mongoose';
+import { DB_NAME } from '../constant.js';
 
 export const connectDB = async () => {
   try {
-    const connectionInstance = await mongoose.connect(``);
+    const connectionInstance = await mongoose.connect(
+      `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.rocppxe.mongodb.net/${DB_NAME}`
+    );
+    console.log('Database connection instance host', connectionInstance.connection.host);
   } catch (error) {
     console.log('DB connection error', error);
     process.exit(1);

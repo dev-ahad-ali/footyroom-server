9b204d91ce467b1721906eae603920dc5aa40f3b
dev-ahad-ali <ahadali.dev@gmail.com>
2024-08-16 01:28:13 +0600

    Update : database connected and express app running succesfully

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-require('dotenv').config({ path: './.env' });
+// require('dotenv').config({ path: './.env' });
+import dotenv from 'dotenv';
+import { connectDB } from './db/db.js';
+import { app } from './app.js';
+
+dotenv.config({
+  path: './.env',
+});
+
+connectDB()
+  .then(() => {
+    app.on('error', (error) => {
+      console.log('Server Initialize error', error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(`Server running on Port : ${process.env.Port || 5000}`);
+    });
+  })
+  .catch((error) => {
+    console.log('Db connection error', error);
+  });

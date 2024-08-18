984d404b6219f32602ea622e53e4b9023e327751
dev-ahad-ali <ahadali.dev@gmail.com>
2024-08-18 23:54:34 +0600

    Update: Get brand name method added

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -32,16 +32,27 @@ const addProduct = methodHandler(async (req, res) => {
 });
 
 const getProduct = methodHandler(async (req, res) => {
-  const { page, limit } = req.query;
+  const { page, limit, search } = req.query;
 
   const skip = (page - 1) * limit;
 
-  const products = await Product.find().skip(skip).limit(limit);
-  const productsCount = products.length;
+  const products = await Product.find({ name: { $regex: search, $options: 'i' } })
+    .skip(skip)
+    .limit(limit);
+
+  const productsCount = await Product.find({
+    name: { $regex: search, $options: 'i' },
+  }).countDocuments();
 
   return res
     .status(200)
     .json(new ApiResponse(200, [products, productsCount], 'All product fetched successfully'));
 });
 
-export { addProduct, getProduct };
+const getBrands = methodHandler(async (_, res) => {
+  const brands = await Product.distinct('brand'); //get a specific field from mongodb only if it has a unique value
+
+  return res.status(200).json(brands);
+});
+
+export { addProduct, getProduct, getBrands };
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { addProduct, getProduct } from '../controllers/product.controller.js';
+import { addProduct, getBrands, getProduct } from '../controllers/product.controller.js';
 
 const ProductRouter = Router();
 
 ProductRouter.route('/add-product').post(addProduct);
 ProductRouter.route('/get-product').get(getProduct);
+ProductRouter.route('/get-brands').get(getBrands);
 
 export { ProductRouter };
